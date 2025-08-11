4da007ab0174f33ac41576f77875a38af067df48
Akshad11 <akshad.dhole14.com>
2025-08-12 00:37:14 +0530

    Route changes

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -9,7 +9,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<RoomLanding />} />
-      <Route path="/rooms/:roomId" element={<RoomEditor />} />
+      <Route path="/room/:roomId" element={<RoomEditor />} />
     </Routes>
   </BrowserRouter>
 );
